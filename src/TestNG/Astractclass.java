c5082850adc39f245f66e87169716b1a3f407745
187IT09835 - TRAN CHI THIEN - T6 <thien.187it09835@vanlanguni.vn>
2021-10-30 08:45:59 +0700

    v2

--- src/TestNG/Astractclass.java
+++ src/TestNG/Astractclass.java
@@ -27,6 +27,6 @@ public class Astractclass {
 	}
 	@AfterTest
 	public void afterTest() {
-		//driver.close();
+		driver.close();
 	}
 }
--- src/TestNG/ForgetPassword.java
+++ src/TestNG/ForgetPassword.java
@@ -5,33 +5,53 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+
 @Listeners (TestNG.Listener.class)
 public class ForgetPassword extends Astractclass {
-	@Test (priority = 8)
-	public static void FP_01() {
-		String url = "https://hoanghamobile.com/Account/ForgotPassword";
-		driver.get(url);
-//		WebElement dn = driver.findElement(By.xpath("//a[text()='Đăng nhập']"));
-//		dn.click();
-//		WebElement cl_quenmk = driver.findElement(By.xpath("//a[@class=\"ajax-content\"]"));
-//		cl_quenmk.click();
-		WebElement nhap_email = driver.findElement(By.xpath("//div[@class='just-center'][2]//input[2]"));
-		nhap_email.clear();
-		nhap_email.sendKeys("Chuabietcode@gmail.com");
-//		driver.findElement(By.xpath("//div[@id=\"close-button-1545222288830\"]")).click();
-		driver.findElement(By.xpath("//div[@class='just-center'][2]//button")).click();
-		WebElement error = driver.findElement(By.xpath("//div[@class=\"form-erros\"]/p/strong"));
-		String kq_mongdoi = "Email Chuabietcode@gmail.com chưa được đăng ký, vui lòng chọn email khác hoặc thực hiện đăng ký tài khoản";
-		String kq_thucte = error.getText();
-		boolean kq;
-		kq = kq_thucte.equals(kq_mongdoi);
-		System.out.println("Kết quả TC FP_01:"+kq);
 	
+	//khai bao du lieu
+	private static final String path_file = "D:\\HK1 2021 -2022\\Automation Testing\\DoAn\\Team08\\data.xlsx";
+	private static final String sheet_name_cp = "Check_Password";
+	public static final String url_quenmk = "https://hoanghamobile.com/Account/ForgotPassword";
+	private static int COL_DATA = 2;
+	private static int COL_MONGDOI = 4;
+	private static int COL_KETQUA = 5;
+	
+	
+	//khai bao cho excel file
+	@Test (priority = 8)
+	public void check_run() throws Exception {
+		ExcelUtils.setExcelFile(path_file, sheet_name_cp);
 	}
 	@Test (priority = 9)
+	public static void FP_01() throws Exception {
+		driver.get(url_quenmk);
+		WebElement nhap_email = driver.findElement(By.xpath("//div[@class='just-center'][2]//input[2]"));
+		WebElement error = driver.findElement(By.xpath("//div[@class=\"form-erros\"]/p/strong"));
+		nhap_email.clear();
+		for(int i = 1; i<=ExcelUtils.getRowUsed(); i++)
+		{
+			String data_1 = ExcelUtils.getCellData(i,COL_DATA);
+			String mongdoi = ExcelUtils.getCellData(i, COL_MONGDOI);
+			//Step 1: input email
+			nhap_email.sendKeys(data_1);
+			//Step 2: get error
+			String thucte = error.getText();
+			//Step 3: click cap lai mk
+			driver.findElement(By.xpath("//div[@class='just-center'][2]//button")).click();
+			//Step 4: so sanh
+			if(mongdoi.equals(thucte)==true) {
+				ExcelUtils.setCellData(i, COL_KETQUA, "PASSED");
+			}
+			else {
+				ExcelUtils.setCellData(i, COL_KETQUA, "Failure");
+			}
+		}
+		System.out.println("Xong TC FP_01");
+	}
+	@Test (priority = 10,enabled = false) // chưa code xong
 	public static void FP_02() {
-		String url = "https://hoanghamobile.com/Account/ForgotPassword";
-		driver.get(url);
+		driver.get(url_quenmk);
 		WebElement mail = driver.findElement(By.xpath("//div[@class='just-center'][2]//input[2]"));
 		mail.clear();
 		mail.sendKeys("");
@@ -44,13 +64,13 @@ public class ForgetPassword extends Astractclass {
 		System.out.println("Kết quả TC FP_02: "+ kq);
 		
 	}
-	@Test (priority = 10,enabled = false) // chưa code xong
+	@Test (priority = 11,enabled = false) // chưa code xong
 	public static void FP_03() {
 		WebElement cl_quenmk = driver.findElement(By.xpath("//a[@class=\"ajax-content\"]"));
 		cl_quenmk.click();
 		WebElement nhap_email = driver.findElement(By.xpath("//div[@class=\"row\"]/input[@id=\"Email\"]"));
 		nhap_email.clear();
-		nhap_email.sendKeys("chithien3203.vk.1718@gmail.com");
+		//nhap_email.sendKeys("chithien3203.vk.1718@gmail.com");
 		driver.findElement(By.xpath("//div[@id=\"close-button-1545222288830\"]")).click();
 		driver.findElement(By.xpath("//button[@class=\"btn btn-block btn-primary\"]")).click();
 		WebElement error = driver.findElement(By.xpath("//div[@class=\"block-content block-content-full block-content-narrow\"]/p"));
